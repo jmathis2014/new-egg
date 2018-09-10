2766c62b959156bba8eac1c3189b99b1a2b7a685
James Mathis <mathis_james@outlook.com>
2018-09-10 18:20:21 -0400

    egg module added.

---
+++ egg/egg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python -W all
+
+import sys
+import os
+import requests
+import time
+import random
+import logging
+import logging.handlers
+
+class egg:
+
+    def __init__(self):
+        pass
+
+    def test(self):
+        print 'Hello this is a test of egg.'
+
